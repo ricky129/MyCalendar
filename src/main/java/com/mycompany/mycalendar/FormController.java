716f01c5e292a093471cf57ba63b34d5c8c3f400
ricky129 <riccardomarchesini036@gmail.com>
2025-03-03 22:45:04 +0100

    code cleanup

--- src/main/java/com/mycompany/mycalendar/FormController.java
+++ src/main/java/com/mycompany/mycalendar/FormController.java
@@ -1,10 +1,8 @@
 package com.mycompany.mycalendar;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.time.Year;
-import java.time.format.DateTimeFormatter;
 import javax.swing.JComboBox;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
@@ -42,58 +40,12 @@ public class FormController {
         return month.length(isLeapYear(currentYear));
     }
 
-    // Helper method to get month index (0-based)
-    private int getMonthIndex(String month) {
-        switch (month) {
-            case "January" -> {
-                return 0;
-            }
-            case "February" -> {
-                return 1;
-            }
-            case "March" -> {
-                return 2;
-            }
-            case "April" -> {
-                return 3;
-            }
-            case "May" -> {
-                return 4;
-            }
-            case "June" -> {
-                return 5;
-            }
-            case "July" -> {
-                return 6;
-            }
-            case "August" -> {
-                return 7;
-            }
-            case "September" -> {
-                return 8;
-            }
-            case "October" -> {
-                return 9;
-            }
-            case "November" -> {
-                return 10;
-            }
-            case "December" -> {
-                return 11;
-            }
-            default ->
-                throw new IllegalArgumentException("Invalid month");
-        }
-    }
-
     public void updateCalendar(JTable JTB1, JComboBox JCB1, Month month) {
 
         // Clear the table
-        for (int row = 0; row < JTB1.getRowCount(); row++) {
-            for (int col = 0; col < 7; col++) {
+        for (int row = 0; row < JTB1.getRowCount(); row++)
+            for (int col = 0; col < 7; col++)
                 ((DefaultTableModel) JTB1.getModel()).setValueAt(null, row, col);
-            }
-        }
 
         Month selectedMonth = Month.of(JCB1.getSelectedIndex() + 1);
         int daysInMonth = getNumberOfDays(selectedMonth);
@@ -122,60 +74,44 @@ public class FormController {
     public boolean getEvent(LocalDateTime date, JTextField EventName, JTextArea EventInfo) {
         boolean ret = false;
         String eventInfoTemp;
+        
         if (LCSV1.getEventi() == null) {
             System.out.println("Event list is null");
             return false;
         }
-
-        System.out.println("Valori data clickata: "
-                + date.getYear() + " "
-                + date.getMonth() + " "
-                + date.getDayOfMonth());
+        
+        if(LCSV1.getEventi().isEmpty()){
+            System.out.println("Event list is empty");
+            return false;
+        }
 
         for (Event evento : LCSV1.eventi) {
             LocalDateTime dateLCSV = evento.date;
 
-            System.out.println("Valori array: "
-                    + dateLCSV.getYear() + " "
-                    + dateLCSV.getMonth() + " "
-                    + dateLCSV.getDayOfMonth());
-            
-            if (dateLCSV.getYear() == date.getYear()
-                    && dateLCSV.getMonth() == date.getMonth()
-                    && dateLCSV.getDayOfMonth() == date.getDayOfMonth()) {
-
-                if(EventName.getText().isEmpty()){
-                    System.out.println("CASO 1");
-                    EventName.setText(evento.name);
-                    EventInfo.setText(dateLCSV.getMonth() + " " + dateLCSV.getDayOfMonth() + "\n" + evento.description + "\n");
-                    ret = true;
-                    continue;
-                }
-                if(!EventInfo.getText().isEmpty() && !EventName.getText().equals("More events")){
-                    eventInfoTemp = EventInfo.getText();
-                    EventInfo.setText(EventName.getText() + "\n" + eventInfoTemp + "\n" + evento.name + "\n" + dateLCSV.getMonth() + " " + dateLCSV.getDayOfMonth() + "\n"+ evento.description + "\n");
-                    EventName.setText("More events");
-                    System.out.println("CASO 2");
-                    ret = true;
-                    continue;
-                }
-                if(EventName.getText().equals("More events")){
-                    EventInfo.append("\n" + evento.name + "\n" + dateLCSV.getMonth() + " " + dateLCSV.getDayOfMonth() + "\n" + evento.description);
-                    System.out.println("CASO 3");
-                    ret = true;
+            if(dateLCSV.toLocalDate().equals(date.toLocalDate())){
+                
+                switch (EventName.getText()) {
+                    case "" -> {
+                        System.out.println("CASO 1");
+                        EventName.setText(evento.name);
+                        EventInfo.setText(dateLCSV.getMonth() + " " + dateLCSV.getDayOfMonth() + "\n" + evento.description + "\n");
+                        ret = true;
+                    }
+                    case "More events" -> {
+                        EventInfo.append("\n" + evento.name + "\n" + dateLCSV.getMonth() + " " + dateLCSV.getDayOfMonth() + "\n" + evento.description);
+                        System.out.println("CASO 3");
+                        ret = true;
+                    }
+                    default -> {
+                        eventInfoTemp = EventInfo.getText();
+                        EventInfo.setText(EventName.getText() + "\n" + eventInfoTemp + "\n" + evento.name + "\n" + dateLCSV.getMonth() + " " + dateLCSV.getDayOfMonth() + "\n" + evento.description + "\n");
+                        EventName.setText("More events");
+                        System.out.println("CASO 2");
+                        ret = true;
+                    }
                 }
             }
         }
         return ret;
-    }
-
-    public int getDayId(int day, String month, int year) {
-        try {
-            LocalDateTime date = LocalDateTime.of(year, getMonthIndex(month) + 1, day, 0, 0);
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMyyyy");
-            return Integer.parseInt(date.format(formatter));
-        } catch (NumberFormatException e) {
-            return -1;
-        }
     }
 }
